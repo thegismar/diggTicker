b58390f6494d1b7386b337dd247013ed8a58d0c8
Gismar <thegismar@gmail.com>
2021-01-22 22:22:04 -0300

    dash

--- chart.py
+++ chart.py
@@ -1,13 +1,20 @@
+import dash
 import plotly.express as px
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objects as go  # or plotly.express as px
 import pandas as pd
 from datetime import datetime
 import time
 
-df = pd.read_csv('DIGG_WBTC.csv', usecols=['Timestamp', 'Price'])
+df = pd.read_csv( 'DIGG_WBTC.csv', usecols=['Timestamp', 'Price'] )
 
-df['Timestamp'] = df['Timestamp'].apply(lambda x: datetime.fromtimestamp(float(x)))
+df['Timestamp'] = df['Timestamp'].apply( lambda x: datetime.fromtimestamp( float( x ) ) )
 
-fig = px.line(df, x=df.Timestamp, y=df.Price)
-while True:
-    fig.show()
-    time.sleep(5)
+fig = go.Figure( data=[go.Scatter( x=df['Timestamp'], y=df['Price'] )] )
+
+dcc.Graph( id='example-graph-2', figure=fig )
+app = dash.Dash()
+app.layout = html.Div( [dcc.Graph( figure=fig )] )
+
+app.run_server( debug=True)
