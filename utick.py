682c44cf7c5364c1f7ab6f43921fe99bc558e412
Gismar <thegismar@gmail.com>
2021-01-23 17:30:50 -0300

    added dotenv

--- utick.py
+++ utick.py
@@ -1,12 +1,16 @@
 from brownie import *
 import time
-import sys
+from dotenv import load_dotenv
+import os
 
-network.connect( 'alchemy' )
+load_dotenv()
 
-i = str(sys.argv[1])
+w3 = os.getenv( 'WEB3_PROVIDER' )
+network.connect( 'w3 ' )
 
-contract = web3.toChecksumAddress(i)
+i = '0x9a13867048e01c663ce8ce2fe0cdae69ff9f35e3'
+
+contract = web3.toChecksumAddress( i )
 contract = Contract.from_explorer( contract )
 
 token1 = contract.token1()
@@ -21,17 +25,20 @@ decimals0 = token0_c.decimals()
 symbol1 = token1_c.symbol()
 symbol0 = token0_c.symbol()
 
-while True:
+
+def get_price():
     reserve1 = contract.getReserves()[1]
     reserve0 = contract.getReserves()[0]
     reserve1 = int( reserve1 ) / 10 ** int( decimals1 )
     reserve0 = int( reserve0 ) / 10 ** int( decimals0 )
-    ts = int(time.time())
+
+    ts = int( time.time() )
     price = reserve0 / reserve1
 
-    print( f'{ts}:  {price:.8f} {symbol1}/{symbol0}' )
+    return f'{ts}:  {price:.8f} {symbol1}/{symbol0}'
 
-    with open( f'{symbol1}_{symbol0}.csv', 'a' ) as f:
-        f.write( f'{ts},{price:.8f} \n' )
 
-    time.sleep( 0.5 )
+if __name__ == '__main__':
+    while True:
+        print( get_price() )
+        time.sleep( 1 )
